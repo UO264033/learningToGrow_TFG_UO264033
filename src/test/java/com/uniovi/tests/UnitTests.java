4b828fc3630e1dccf5d24ae085013b20a5f8bf0b
uo264033 <uo264033@uniovi.es>
2021-06-19 09:44:40 +0200

    ya va?

--- src/test/java/com/uniovi/tests/UnitTests.java
+++ src/test/java/com/uniovi/tests/UnitTests.java
@@ -1,37 +1,47 @@
 package com.uniovi.tests;
 
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.uniovi.entities.Exercise;
+import com.uniovi.repositories.ExerciseRepository;
+import com.uniovi.services.ExerciseService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringRunner.class)
+@SpringBootApplication
 class UnitTests {
 	
-//	@Before
-//	public void setUp() throws Exception {
-//		exerciseService = new ExerciseService();
-//	}
-//
-//	@Autowired
-//    private ExerciseService exerciseService;
-//	
-//	@Autowired
-//	private ExerciseRepository exerciseRepository;
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Autowired
+    private ExerciseService exerciseService;
+	
+	@Autowired
+	private ExerciseRepository exerciseRepository;
  
-//	@Test
-//	void test() {
+	@Test
+	void test() {
 //		ServicioA servicioA=mock(ServicioA.class);
 //		when(servicioA.sumar(2,3)).thenReturn(5);
-//		Exercise exercise = new Exercise();
-//		exercise.setName("E1");
-//		exerciseService.addExercise(exercise);
+		Exercise exercise = new Exercise();
+		exercise.setName("E1");
+		exerciseService.addExercise(exercise);
 //		when(this.exerciseRepository.findByName("E1")).thenReturn(exercise);
 //		 assertEquals(exercise.getName(), exerciseService.getExerciseByName("E1").getName());
-//	}
+	}
 	
-//	@Test
-//	void test2() {
+	@Test
+	void test2() {
 //		assertEquals("E1", exerciseService.getExerciseByName("E1"));
-//	}
+	}
 
 }
